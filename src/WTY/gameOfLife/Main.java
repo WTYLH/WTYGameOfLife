a8f63dca4905fd971073781165c85398a3af432d
WTY <1248433404@qq.com>
2022-06-09 14:41:52 +0800

    生命游戏提交

---
+++ src/WTY/gameOfLife/Main.java
@@ -0,0 +1,12 @@
+package WTY.gameOfLife;
+
+import WTY.gameOfLife.ui.GameOfLifeFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new GameOfLifeFrame();
+
+    }
+}
